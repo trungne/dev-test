976fcf80cb6b9960440fdb98351c30f54aae315b
trungne <nguyenquochoangtrung@gmail.com>
2022-10-03 19:46:50 +0700

    chore: add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ module.exports = {
     extend: {
       colors: {
         neutral: '#232323',
+        ['neutral-1']: '#FFFFFF',
+        ['neutral-3']: '#F1F1F1',
         ['neutral-4']: '#E3E3E3',
+        ['neutral-5']: '#B4B4B4',
+        ['neutral-6']: '#8C8C8C',
+        ['neutral-7']: '#5F5F5F',
+        ['neutral-8']: '#232323',
         ['carbuyer-primary']: '#EE1B24',
       }
     },
