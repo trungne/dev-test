f94a08ebc31097bee8f9e09fd2d96c4e3682f0c9
trungne <nguyenquochoangtrung@gmail.com>
2022-10-04 12:11:29 +0700

    chore: add car list

--- components/CarList/CarList.tsx
+++ components/CarList/CarList.tsx
@@ -1,6 +1,9 @@
+import UnstyledButton from "components/Common/UnstyledButton";
 import React from "react";
+import { CDN_HOST } from "shared/constants";
 import { CarInfo } from "shared/types";
 import { useGetCarInfoQuery } from "store/carapi.slice";
+import InfoCard, { AdvertiseCard } from "./InfoCard";
 
 type Props = {
     defaultCarData: CarInfo[]
@@ -8,11 +11,44 @@ type Props = {
 const CarList: React.FC<Props> = ({ defaultCarData }) => {
     // refetch car data to make sure that the data displayed is the latest one
     const { data } = useGetCarInfoQuery()
-    console.log(data)
-    return <div>
-        {JSON.stringify(defaultCarData)}
-        {JSON.stringify(data)}
-    </div>
+
+
+    return (
+        <>
+            <div className="grid place-items-center overflow-x-auto overflow-hidden
+        ">
+                <div className="
+    mt-10 md:mt-40
+    px-3 sm:px-6 md:px-10 lg:px-14
+    grid grid-rows-1 grid-flow-col gap-x-10
+    place-items-center
+    md:grid-flow-row
+    md:gap-y-6
+    md:grid-cols-2
+    lg:grid-cols-3 xl:grid-cols-4
+    xl:max-w-[1242px]
+    ">
+                    <AdvertiseCard imageSrc={CDN_HOST + 'car_ad.png'} />
+                    {defaultCarData.map(info => <InfoCard key={info.name + info.brandName + info.photoURL} carInfo={info} />)}
+                </div>
+            </div>
+            <div className="flex mt-6 px-3">
+                <UnstyledButton className="bg-white 
+                font-['Poppins']
+                font-normal
+                ml-auto
+                md:m-auto
+                rounded 
+                border !border-solid border-carbuyer-primary 
+                text-base leading-6 text-carbuyer-primary  
+                px-4 py-2">
+                    View more new cars
+                </UnstyledButton>
+            </div>
+
+        </>
+
+    )
 }
 
 export default CarList
--- components/CarList/InfoCard.tsx
+++ components/CarList/InfoCard.tsx
@@ -1,8 +1,49 @@
 import React from "react";
 import { Paper } from '@mantine/core'
-const InfoCard: React.FC = () => {
-    return <Paper className="w-[281px] h-[361px]" shadow="sm">
+import { CarInfo } from "shared/types";
+import Image from "next/image";
 
+
+export const AdvertiseCard: React.FC<{ imageSrc: string }> = ({ imageSrc }) => {
+    return (
+        <Paper className="w-[281px] relative h-[361px] cursor-pointer" shadow="sm">
+            <Image objectFit="contain" alt="advertise" src={imageSrc} layout="fill" />
+        </Paper>
+    )
+}
+
+type Props = {
+    carInfo: CarInfo
+}
+const InfoCard: React.FC<Props> = ({ carInfo }) => {
+    return <Paper key={carInfo.name + carInfo.brandName + carInfo.photoURL} className="w-[281px] h-[361px] cursor-pointer" shadow="sm">
+        <div className="relative bg-white border-b border-b-solid border-neutral-4 w-full h-[187px]">
+            <Image objectFit="contain" alt={`${carInfo.name} photo`} src={carInfo.photoURL} layout="fill" />
+        </div>
+        <div className="pt-4 px-4 pb-6 font-['Poppins']">
+            <div className="mb-2">{carInfo.name}</div>
+            <div className="flex gap-2 mb-2 items-center">
+                <span className=" text-base leading-6 text-neutral-8 font-normal">
+                    {
+                        carInfo.totalPrice ?
+                            <>From <span className="text-secondary-main">{carInfo.totalPrice}</span></> :
+                            <>POA</>
+                    }
+                </span>
+                <span className="rounded-sm bg-secondary-light py-[2px] px-1 text-secondary-main font-normal leading-5">
+                    ${carInfo.pricePerMonth}/mo
+                </span>
+            </div>
+            <div className="flex gap-1 mb-4 text-neutral-7 text-sm leading-[22px] font-normal">
+                <span>{carInfo.variants}</span> {carInfo.variants === 1 ? "variant" : "variants"}
+                {carInfo.withCOE && <span>| with COE</span>}
+            </div>
+
+            <div className="flex gap-2 items-center">
+                <Image className="rounded-full" alt={`${carInfo.name}'s logo`} width={32} height={32} src={carInfo.brandLogo} />
+                <span className="font-normal text-sm leading-[22px] text-neutral-8">{carInfo.brandName}</span>
+            </div>
+        </div>
     </Paper>
 }
 
