650752bb1e92f7912e05ca5d936f5ce0952226e9
trungne <nguyenquochoangtrung@gmail.com>
2022-10-04 16:23:01 +0700

    chore: fix styling

--- components/AskSection/AskSection.tsx
+++ components/AskSection/AskSection.tsx
@@ -6,14 +6,13 @@ import UnstyledButton from "components/Common/UnstyledButton";
 import Link from "next/link";
 const AskSection: React.FC = () => {
     return (
-        <div className="relative grid 
-        justify-items-center md:justify-items-start
+        <div className="relative 
          font-['Poppins'] mt-[132px] min-h-[694px] px-6 md:px-20">
             <div className="absolute top-0 left-0 w-full h-full z-[-1]">
                 <Image objectFit="cover" layout="fill" alt="background" src={getCDNImage(isMobile ? 'background_2_mobile.png' : 'background_2_pc.png')} />
             </div>
-            <div className="md:max-w-[410px]">
-                <h1 className="mt-[80px] mb-[186px] md:mb-[18px] max-w-[288px] whitespace-pre 
+            <div className="max-w-full w-full md:max-w-[410px] pt-[80px] grid justify-items-center md:justify-items-start">
+                <h1 className="mt-0 mb-[186px] md:mb-[18px] max-w-[288px] whitespace-pre 
         text-[48px] md:text-[76px]
         leading-[72px] md:leading-[114px]
         font-semibold
