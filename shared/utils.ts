bc53cfff043815320fc7ee507b0af6bc2e019e90
trungne <nguyenquochoangtrung@gmail.com>
2022-10-05 13:34:36 +0700

    chore: add detect mobile util function

--- shared/utils.ts
+++ shared/utils.ts
@@ -4,3 +4,20 @@ import { CDN_HOST } from "./constants";
 export const getCDNImage = (path: string) => {
     return CDN_HOST + path
 }
+
+// https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+export const detectMobile = (userAgent: string) => {
+    const toMatch = [
+        /Android/i,
+        /webOS/i,
+        /iPhone/i,
+        /iPad/i,
+        /iPod/i,
+        /BlackBerry/i,
+        /Windows Phone/i
+    ];
+
+    return toMatch.some((toMatchItem) => {
+        return userAgent.match(toMatchItem);
+    });
+}
