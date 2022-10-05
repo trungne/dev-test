4427ea9e4de6350b31781554d1992e7acd3d09c9
trungne <nguyenquochoangtrung@gmail.com>
2022-10-06 00:41:11 +0700

    chore: add update car brand

--- components/Dashboard/CarBrandTab.tsx
+++ components/Dashboard/CarBrandTab.tsx
@@ -29,7 +29,7 @@ const AddCarBrandModal: React.FC<{ opened: boolean, onClose: () => void }> = ({ 
             </div>
             <Divider />
             <div className="p-6">
-                <CommonBrandDetail isEditMode={true} />
+                <CommonBrandDetail mode='create' onCreate={onClose} onClose={onClose} />
 
                 <div id="add-car-brand-control-buttons" className="flex justify-end items-end gap-4 mt-[34px]">
                     <UnstyledButton
@@ -47,119 +47,175 @@ const AddCarBrandModal: React.FC<{ opened: boolean, onClose: () => void }> = ({ 
     )
 }
 
-const CommonBrandDetail: React.FC<{ brand?: CarBrand, isEditMode: boolean }> = ({ brand, isEditMode }) => {
+type Mode = 'edit' | 'create' | 'read'
+
+type CommonBrandDetailProps = {
+    brand?: CarBrand
+    mode: Mode
+    onClose?: () => void
+    onEdit?: () => void
+    onCreate?: () => void
+}
+const CommonBrandDetail: React.FC<CommonBrandDetailProps> = ({ brand, mode, onClose, onCreate, onEdit }) => {
     const [opened, setOpened] = React.useState(false)
+
+
+    const [updateCarBrand] = useUpdateCarBrandsMutation()
+
+    const [newCarBrand, setNewCarBrand] = React.useState<Partial<CarBrand>>({ ...brand })
+
     const activeColor = React.useMemo(() => {
-        if (brand === undefined) {
+        if (mode === 'create') {
             return { background: '#CEF7E2', text: '#1F7B4D' } // assume active
         }
-        return brand.isActive ? { background: '#CEF7E2', text: '#1F7B4D' } : { background: '#FAFAFA', text: '#5F5F5F' }
-    }, [brand])
+        return newCarBrand?.isActive ? { background: '#CEF7E2', text: '#1F7B4D' } : { background: '#FAFAFA', text: '#5F5F5F' }
+    }, [newCarBrand, mode])
 
     return (
         <>
-            <div>
-                <div className="py-3 text-sm leading-6 text-neutral-8 font-bold">
-                    Brand Logo
-                </div>
+            <div className="py-3 text-sm leading-6 text-neutral-8 font-bold">
+                Brand Logo
+            </div>
 
-                <Divider />
+            <Divider />
 
-                <div className="mt-4 relative w-[120px] h-[120px]">
-                    {isEditMode && brand && <div className="absolute cursor-pointer px-4 flex items-center justify-center rounded-full
+            <div className="mt-4 relative w-[120px] h-[120px]">
+                {mode === 'edit' && brand && <div className="absolute cursor-pointer px-4 flex items-center justify-center rounded-full
                     w-full h-full left-0 top-0 bg-neutral-8 opacity-0 hover:opacity-80 z-10
                     text-base text-white text-center
                     ">
-                        CHANGE LOGO
+                    CHANGE LOGO
+                </div>}
+                {mode === 'create' &&
+                    <div className="flex flex-col cursor-pointer rounded-full items-center justify-center w-full h-full outline-dashed outline-neutral-4 bg-neutral-2">
+                        <IconPlus width={32} height={32} color="#232323" />
+                        <div className="mt-1 text-neutral-6 text-xs leading-5 font-bold">Brand Logo</div>
                     </div>}
-                    {!brand &&
-                        <div className="flex flex-col cursor-pointer rounded-full items-center justify-center w-full h-full outline-dashed outline-neutral-4 bg-neutral-2">
-                            <IconPlus width={32} height={32} color="#232323" />
-                            <div className="mt-1 text-neutral-6 text-xs leading-5 font-bold">Brand Logo</div>
-                        </div>}
-                    {brand && <Image src={brand.logoURL} alt="brand logo" layout="fill" />}
-                </div>
+                {newCarBrand && <Image src={newCarBrand.logoURL!} alt="brand logo" layout="fill" />}
             </div>
 
 
-            <div>
-                <div className="mt-6 py-4 text-sm leading-6 text-neutral-8 font-bold">
-                    Brand Details
-                </div>
-                <Divider />
-                <div className="flex flex-wrap">
-                    <div className='mr-0 md:mr-8 basis-60 mt-4'>
-                        <div className="text-neutral-6 text-sm leading-[22px] font-normal">
-                            Brand Name
-                        </div>
-                        <div className="text-neutral-8 mt-1 text-sm leading-[22px] font-bold">
-                            {isEditMode ? <TextInput classNames={{
+            <div className="mt-6 py-4 text-sm leading-6 text-neutral-8 font-bold">
+                Brand Details
+            </div>
+            <Divider />
+            <div className="flex flex-wrap">
+                <div className='mr-0 md:mr-8 basis-60 mt-4'>
+                    <div className="text-neutral-6 text-sm leading-[22px] font-normal">
+                        Brand Name
+                    </div>
+                    <div className="text-neutral-8 mt-1 text-sm leading-[22px] font-bold">
+                        {mode !== 'read' ? <TextInput
+                            onChange={(e) => { setNewCarBrand(prev => { return { ...prev, name: e.currentTarget.value } }) }}
+                            classNames={{
                                 input: "py-[9px] px-[14px] text-sm leading-[22px] font-semibold h-[40px]"
-                            }} value={brand?.name} placeholder="Input Content" /> :
-                                <div className="py-[9px]">
-                                    {brand?.name}
-                                </div>}
-                        </div>
-                    </div>
-
-                    <div className='basis-29 mt-4'>
-                        <div className="text-neutral-6 text-sm leading-[22px] font-normal">
-                            Brand Status
-                        </div>
-                        <Popover opened={opened} onChange={setOpened}>
-                            <Popover.Target>
-                                <div
-                                    onClick={() => { setOpened(o => !o) }}
-                                    style={{
-                                        backgroundColor: activeColor.background,
-                                        color: activeColor.text,
-                                        cursor: isEditMode ? 'pointer' : undefined
-                                    }} className="flex items-center gap-2 rounded-full py-[5px] px-3 text-base font-semibold mt-[10px]">
-                                    <Radio fill={activeColor.text} />
-                                    {!brand ? 'Active' : brand.isActive ? 'Active' : 'Inactive'}
-                                    {isEditMode && <IconChevronDown width={24} height={24} color="#232323" />}
-                                </div>
-                            </Popover.Target>
-                            <Popover.Dropdown p="xs">
-                                <div className="flex flex-col justify-center gap-3">
-                                    <div
-                                        className="cursor-pointer py-[5px] px-3 flex items-center gap-2 rounded-full text-base font-semibold text-primary-dark-3 bg-primary-light-1">
-                                        <Radio fill="#1F7B4D" />
-                                        Active
-                                    </div>
-
-                                    <div
-                                        className="cursor-pointer py-[5px] px-3 flex items-center gap-2 rounded-full text-base font-semibold text-neutral-7 bg-neutral-2">
-                                        <Radio fill="#5F5F5F" />
-                                        Inactive
-                                    </div>
-                                </div>
-                            </Popover.Dropdown>
-                        </Popover>
-
-                    </div>
-
-                    <div className="basis-full mt-4">
-                        <div className="text-neutral-6 text-sm leading-[22px] font-normal">
-                            Brand Description
-                        </div>
-                        <div className="text-neutral-8 mt-1 text-sm leading-[22px] font-bold h-[100px]">
-                            {isEditMode ? <Textarea classNames={{
-                                input: "py-[9px] px-[14px] text-sm leading-[22px] font-semibold h-[100px]"
-                            }} value={brand?.description} placeholder="Input Content" /> : <div className="py-[9px]">
-                                {brand?.description}
+                            }} value={newCarBrand?.name} placeholder="Input Content" /> :
+                            <div className="py-[9px]">
+                                {newCarBrand?.name}
                             </div>}
+                    </div>
+                </div>
 
-                        </div>
+                <div className='basis-29 mt-4'>
+                    <div className="text-neutral-6 text-sm leading-[22px] font-normal">
+                        Brand Status
+                    </div>
+                    <Popover opened={opened} onChange={setOpened}>
+                        <Popover.Target>
+                            <div
+                                onClick={() => { setOpened(o => !o) }}
+                                style={{
+                                    backgroundColor: activeColor.background,
+                                    color: activeColor.text,
+                                    cursor: mode !== 'read' ? 'pointer' : undefined
+                                }} className="flex items-center gap-2 rounded-full py-[5px] px-3 text-base font-semibold mt-[10px]">
+                                <Radio fill={activeColor.text} />
+                                {!newCarBrand ? 'Active' : newCarBrand.isActive ? 'Active' : 'Inactive'}
+                                {mode !== 'read' && <IconChevronDown width={24} height={24} color="#232323" />}
+                            </div>
+                        </Popover.Target>
+                        <Popover.Dropdown p="xs">
+                            <div className="flex flex-col justify-center gap-3">
+                                <div
+                                    onClick={() => {
+                                        setNewCarBrand(prev => { return { ...prev, isActive: true } })
+                                    }}
+                                    className="cursor-pointer py-[5px] px-3 flex items-center gap-2 rounded-full text-base font-semibold text-primary-dark-3 bg-primary-light-1">
+                                    <Radio fill="#1F7B4D" />
+                                    Active
+                                </div>
+
+                                <div
+                                    onClick={() => {
+                                        setNewCarBrand(prev => { return { ...prev, isActive: false } })
+                                    }}
+                                    className="cursor-pointer py-[5px] px-3 flex items-center gap-2 rounded-full text-base font-semibold text-neutral-7 bg-neutral-2">
+                                    <Radio fill="#5F5F5F" />
+                                    Inactive
+                                </div>
+                            </div>
+                        </Popover.Dropdown>
+                    </Popover>
+
+                </div>
+
+                <div className="basis-full mt-4">
+                    <div className="text-neutral-6 text-sm leading-[22px] font-normal">
+                        Brand Description
+                    </div>
+                    <div className="text-neutral-8 mt-1 text-sm leading-[22px] font-bold h-[100px]">
+                        {mode !== 'read' ? <Textarea
+
+                            onChange={(e) => { setNewCarBrand(prev => { return { ...prev, description: e.currentTarget.value } }) }}
+                            classNames={{
+                                input: "py-[9px] px-[14px] text-sm leading-[22px] font-semibold h-[100px]"
+                            }} value={newCarBrand?.description} placeholder="Input Content" /> : <div className="py-[9px]">
+                            {newCarBrand?.description}
+                        </div>}
+
                     </div>
                 </div>
             </div>
+
+            {mode === 'create' && <div id="add-car-brand-control-buttons" className="flex justify-end items-end gap-4 mt-[34px]">
+                <UnstyledButton
+                    onClick={onClose}
+                    className="
+                rounded-[4px] py-[9px] px-4 
+                bg-transparent 
+                outline outline-1 outline-neutral-5 
+                text-neutral-8 text-sm leading-[22px] font-medium">Cancel</UnstyledButton>
+                <UnstyledButton onClick={() => {
+                    onCreate?.()
+                }} className="rounded-[4px] py-[9px] px-4 bg-secondary-main 
+                text-white text-sm leading-[22px] font-medium">Create Brand</UnstyledButton>
+            </div>}
+
+            {mode !== 'create' && <UnstyledButton onClick={() => {
+                if (mode === 'edit') {
+                    onEdit?.()
+                    let canEdit = true
+                    Object.keys(newCarBrand).forEach(key => {
+                        if (newCarBrand[key as keyof CarBrand] === undefined) {
+                            canEdit = false
+                        }
+                    })
+
+                    if (canEdit) {
+                        // @ts-ignore
+                        updateCarBrand(newCarBrand)
+                    }
+                }
+                else {
+                    onClose?.()
+                }
+            }} className="bg-secondary-main py-[9px] px-4 text-white rounded-[4px] mt-6 text-sm leading-[22px] font-semibold">{mode === 'edit' ? 'Save Changes' : 'Edit Information'}</UnstyledButton>}
         </>
     )
 }
 
 const CarBrandDetail: React.FC<{ brand?: CarBrand, back: () => void }> = ({ brand, back }) => {
-    const [isEditMode, setIsEditMode] = React.useState(!brand)
+    const [mode, setMode] = React.useState<Mode>('read')
     const [updateCarBrand, { }] = useUpdateCarBrandsMutation()
 
     return (
@@ -169,18 +225,13 @@ const CarBrandDetail: React.FC<{ brand?: CarBrand, back: () => void }> = ({ bran
                 <span className="text-primary-dark-1 text-[24px] leading-[32px] font-bold">Brand Details</span>
             </div>
             <div className="mt-4">
-                <CommonBrandDetail brand={brand} isEditMode={isEditMode} />
-
+                <CommonBrandDetail brand={brand} mode={mode}
+                    onClose={() => setMode('edit')}
+                    onEdit={() => setMode('read')}
+                />
             </div>
 
-            <UnstyledButton onClick={() => {
-                if (!isEditMode) {
-                    setIsEditMode(true)
-                    return
-                }
 
-                setIsEditMode(false)
-            }} className="bg-secondary-main py-[9px] px-4 text-white rounded-[4px] mt-6 text-sm leading-[22px] font-semibold">{isEditMode ? 'Save Changes' : 'Edit Information'}</UnstyledButton>
         </div>
     )
 }
--- pages/api/update-car-brand.ts
+++ pages/api/update-car-brand.ts
@@ -13,9 +13,40 @@ export default function handler(
         return
     }
 
-    // const fileData = fs.readFileSync('server/car-brand.json', 'utf-8')
-    // const jsonData = JSON.parse(fileData)
-    // console.log(jsonData)
-    console.log('body', req.body)
-    // res.status(200).json({ data: carBrandsJSON, msg: 'success' })
+    if (!req.body) {
+        res.status(400).end('Bad request!')
+        return
+    }
+
+    const fileData = fs.readFileSync('server/car-brand.json', 'utf-8')
+    const jsonData = JSON.parse(fileData) as CarBrand[]
+
+    if (!jsonData || Object.keys(jsonData).length === 0) {
+        res.status(500).end('Bad request!')
+        return
+    }
+
+    const newCarBrandData = req.body as CarBrand
+    let hasUpdated = false
+    for (let i = 0; i < jsonData.length; i++) {
+        if (jsonData[i].id === newCarBrandData.id) {
+            jsonData[i] = newCarBrandData
+            hasUpdated = true
+            break
+        }
+    }
+
+    if (!hasUpdated) {
+        res.status(500).end('Object not exist!')
+        return
+    }
+    try {
+        fs.writeFileSync('server/car-brand.json', JSON.stringify(jsonData))
+        res.status(200).json({ data: 'update successful', msg: 'success' })
+        return
+    }
+    catch {
+        res.status(500).end('Cannot update!')
+        return
+    }
 }
